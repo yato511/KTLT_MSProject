a1ec250d7c515aa358491ce70c75700d830f2606
yato511 <37733986+yato511@users.noreply.github.com>
2018-04-24 02:13:21 +0700

    Đợi xong mới viết Header

---
+++ Header.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+struct SinhVien {
+	wchar_t *MSSV;
+	wchar_t *HoVaTen;
+	wchar_t *Faculty;
+	int	Khoa;
+	wchar_t *NgaySinh;
+	wchar_t *Email;
+	wchar_t *HinhAnh;
+	wchar_t *Mota;
+	wchar_t **SoThich;
+	int HobbyCount;
+};
+typedef struct SinhVien SV;
+
+void MemoryDelete(SV &a);	//Xóa vùng nhớ các con trỏ trong kiểu SV
+void PrintSV(SV a);	//In thông tin SV ra màn hình
